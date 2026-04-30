13fb4ef0245a101a78d9375cf4ec80c3dcdd19c0
royal9619@gmail.com <naknin-byvpyg-0pIcra>
2026-04-30 16:56:36 -0600

    @values

--- src/main/java/com/alan/curso/springboot_webapp/SpringbootWebappApplication.java
+++ src/main/java/com/alan/curso/springboot_webapp/SpringbootWebappApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootWebappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootWebappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebappApplication.class, args);
+    }
 
 }
--- src/main/java/com/alan/curso/springboot_webapp/controllers/PathVariableController.java
+++ src/main/java/com/alan/curso/springboot_webapp/controllers/PathVariableController.java
@@ -2,14 +2,40 @@ package com.alan.curso.springboot_webapp.controllers;
 
 import com.alan.curso.springboot_webapp.dto.ParamDto;
 import com.alan.curso.springboot_webapp.models.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
+
+    @Value("${config.username}")
+    private String username;
+    //    @Value("${config.message}")
+//    private String message;
+    @Value("${config.listOfValue}")
+    private List<String> listOfValue;
+    @Value("${config.code}")
+    private Integer code;
+
+    @Value("#{ '${config.listOfValue}'.toUpperCase.split(',') }")
+    private List<String> valueList;
+
+    @Value("#{${config.valuesMap}}")
+    private Map<String, Object> valuesMap;
+    @Value("#{ ${config.valuesMap}.product}")
+    private String product;
+    @Value("#{ ${config.valuesMap}.price}")
+    private String price;
+
+    @Value("#{ '${config.listOfValue}'}")
+    private String valueString;
+
+
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable() String message) {
         ParamDto param = new ParamDto();
@@ -29,5 +55,20 @@ public class PathVariableController {
     public User create(@RequestBody User user) {
         user.setName(user.getName().toUpperCase());
         return user;
+    }
+
+    @GetMapping("/values")
+    public Map<String, Object> values(@Value("${config.message}") String message) {
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("code", code);
+        json.put("message", message);
+        json.put("listOfValue", listOfValue);
+        json.put("valueList", valueList);
+        json.put("valueString", valueString);
+        json.put("valueMap", valuesMap);
+        json.put("product", product);
+        json.put("price", price);
+        return json;
     }
 }
